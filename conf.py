b42dae3c4065c23977f360e99b64dbbb0a529a67
Bernd Ahlers <bernd@graylog.com>
2015-02-13 14:14:25 +0100

    Fix company name. (missing ".")

--- conf.py
+++ conf.py
@@ -57,7 +57,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Graylog'
-copyright = u'2015 Graylog, Inc'
+copyright = u'2015 Graylog, Inc.'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -229,7 +229,7 @@ latex_elements = {
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
   ('index', 'Graylog.tex', u'Graylog Documentation',
-   u'Graylog, Inc', 'manual'),
+   u'Graylog, Inc.', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -259,7 +259,7 @@ latex_documents = [
 # (source start file, name, description, authors, manual section).
 man_pages = [
     ('index', 'graylog', u'Graylog Documentation',
-     [u'Graylog, Inc'], 1)
+     [u'Graylog, Inc.'], 1)
 ]
 
 # If true, show URL addresses after external links.
@@ -273,7 +273,7 @@ man_pages = [
 #  dir menu entry, description, category)
 texinfo_documents = [
   ('index', 'Graylog', u'Graylog Documentation',
-   u'Graylog, Inc', 'Graylog', 'One line description of project.',
+   u'Graylog, Inc.', 'Graylog', 'One line description of project.',
    'Miscellaneous'),
 ]
 
@@ -294,9 +294,9 @@ texinfo_documents = [
 
 # Bibliographic Dublin Core info.
 epub_title = u'Graylog'
-epub_author = u'Graylog, Inc'
-epub_publisher = u'Graylog, Inc'
-epub_copyright = u'2015 Graylog, Inc'
+epub_author = u'Graylog, Inc.'
+epub_publisher = u'Graylog, Inc.'
+epub_copyright = u'2015 Graylog, Inc.'
 
 # The basename for the epub file. It defaults to the project name.
 #epub_basename = u'Graylog'
