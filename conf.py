1b70cffb310fa37acb81e02ac2aa712bdb7c01b9
Dan Torrey <dan@graylog.com>
2018-12-14 13:01:12 -0600

    Another attempt: Revert unintended whitespace changes to conf.py

--- conf.py
+++ conf.py
@@ -211,25 +211,25 @@ htmlhelp_basename = 'Graylogdoc'
 # -- Options for LaTeX output ---------------------------------------------
 
 latex_elements = {
-    # The paper size ('letterpaper' or 'a4paper').
-    #'papersize': 'letterpaper',
+# The paper size ('letterpaper' or 'a4paper').
+#'papersize': 'letterpaper',
 
-    # The font size ('10pt', '11pt' or '12pt').
-    #'pointsize': '10pt',
+# The font size ('10pt', '11pt' or '12pt').
+#'pointsize': '10pt',
 
-    # Additional stuff for the LaTeX preamble.
-    #'preamble': '',
+# Additional stuff for the LaTeX preamble.
+#'preamble': '',
 
-    # Latex figure (float) alignment
-    #'figure_align': 'htbp',
+# Latex figure (float) alignment
+#'figure_align': 'htbp',
 }
 
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    ('index', 'Graylog.tex', u'Graylog Documentation',
-     u'Graylog, Inc.', 'manual'),
+  ('index', 'Graylog.tex', u'Graylog Documentation',
+   u'Graylog, Inc.', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -272,9 +272,9 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    ('index', 'Graylog', u'Graylog Documentation',
-     u'Graylog, Inc.', 'Graylog', 'One line description of project.',
-     'Miscellaneous'),
+  ('index', 'Graylog', u'Graylog Documentation',
+   u'Graylog, Inc.', 'Graylog', 'One line description of project.',
+   'Miscellaneous'),
 ]
 
 # Documents to append as an appendix to all manuals.
