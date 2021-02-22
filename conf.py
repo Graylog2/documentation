36d4574eb9874945a82d827b8bd757171d2db561
Linus Pahl <linus@graylog.com>
2021-02-22 17:31:01 +0100

    Reverting config `html_theme_options` and hiding company name in header by using CSS.

--- conf.py
+++ conf.py
@@ -120,9 +120,7 @@ pygments_style = 'sphinx'
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-html_theme_options = {
-  'logo_only': True,
-}
+#html_theme_options = {}
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
