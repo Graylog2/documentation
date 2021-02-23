8b5cbf5e6264c600d456c7de0eb497c08a99ce9b
Linus Pahl <linus@graylog.com>
2021-02-23 11:07:56 +0100

    Hiding company name in header by using CSS.

--- conf.py
+++ conf.py
@@ -120,10 +120,7 @@ pygments_style = 'sphinx'
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-html_theme_options = {
-  'style_nav_header_background': '#1F1F1F',
-  'logo_only': True,
-}
+#html_theme_options = {}
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
